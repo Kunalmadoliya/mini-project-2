e48a107f1db31151a702ba614b9dbaa716a4ecf9
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-05-19 00:30:48 +0530

    let see

--- stopwatch/src/App.tsx
+++ stopwatch/src/App.tsx
@@ -1,41 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [time,setTime] = useState(0);
-  const [start,setStart] = useState(false);
-  const [pause,setPause] = useState(false);
-
-  const handleTime = () =>{
-      setStart(true)
-    
-
-      if(start){
-      const id  = setInterval(() => {
-       setTime(prevTime => prevTime + 1);
-      }, 1000);
-      }else {
-        clearInterval(id)
-      }
-     
-
-  }
-
-
-  // const handleReset = () =>{
-  //   setStart(false)
-  //   setTime(0)
-  //   clearInterval(handleTime)
-  //   clearInterval(handlePause)
-  // }
-
   return (
-    <>
-       <h1>STOPWATCH</h1>
-      {start && <p>{time}</p>}
-       <button onClick={handleTime}>Start</button>
-       <button onClick={handlePause}>Pause</button>
-       <button onClick={()=>{}}>Reset</button>
-    </>
+    <div>
+      
+    </div>
   )
 }
 
